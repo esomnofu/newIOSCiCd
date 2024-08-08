c03f0f2e129fd11ba1acc1dc81ab103d07e95737
Christian Esomnofu <christian.esomnofu@capricorndigi.com>
2024-08-08 08:35:17 +0100

    ref: bootsrap project

---
+++ newIOSCiCd/newIOSCiCdApp.swift
@@ -0,0 +1,17 @@
+//
+//  newIOSCiCdApp.swift
+//  newIOSCiCd
+//
+//  Created by MAC on 08/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct newIOSCiCdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
